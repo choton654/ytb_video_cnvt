c06a4acaf994e37344e37f785c619801f1d46abc
choton654 <choton654@gmail.com>
2024-01-18 20:27:47 +0530

    delete audio files after success

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var path = require("path");
 var OpenAI = require("openai");
 var axios = require("axios");
 const cp = require('child_process');
-const readline = require('readline');
 const app = express();
 
 var http = require("http").createServer(app);
@@ -23,14 +22,6 @@ var clientGlob = null;
 const openai = new OpenAI({
   apiKey: ''
 });
-// const chunkSize = 1000;
-// const chunkSize = 23 * 1024 * 1024;
-// const tracker = {
-//   start: Date.now(),
-//   audio: { downloaded: 0, total: Infinity },
-//   video: { downloaded: 0, total: Infinity },
-//   merged: { frame: 0, speed: '0x', fps: 0 },
-// };
 const sourceAudio = path.join('audio.mp3')
 const outputAudio = path.join('public', 'audio', 'audio-segment_%03d.mp3')
 const directoryPath = path.join(__dirname, 'public', 'audio');
@@ -38,7 +29,7 @@ const directoryPath = path.join(__dirname, 'public', 'audio');
 getAudio = async (videoURL, res) => {
   console.log(videoURL);
   try {
-    const transferVideo = (chunkname, start, end, isBigSize) => new Promise((resolve, reject) => {
+    const transferVideo = () => new Promise((resolve, reject) => {
       try {
 
         const stream = ytdl(videoURL, {
@@ -128,6 +119,9 @@ getAudio = async (videoURL, res) => {
                 const loopLength = audioFiles.length
                 async function doSomething(n) {
                   if(n === 0) {
+                    fs.unlink('audio.mp3',(err) => {
+                      if (err) throw err;
+                    })
                     console.log("TASK COMPLETED!")
                     return
                   }
@@ -136,6 +130,9 @@ getAudio = async (videoURL, res) => {
                     file: fs.createReadStream(path.join(__dirname, "public", "audio", audioFiles[n-1])),
                   })
                   console.log('---transcription---',audioFiles[n-1],transcription);
+                  fs.unlink(path.join(__dirname, "public", "audio", audioFiles[n-1]), (err) => {
+                    if (err) throw err;
+                  });
                   console.log("I'm doing something.")
                   doSomething(n - 1)
                 }
@@ -184,141 +181,3 @@ io.on("connection", (client) => {
 http.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
-// var stream;
-// if (isBigSize) {
-//   console.log(start, end);
-//   stream = ytdl(videoURL, {
-//     quality: "highestaudio",
-//     filter: "audioonly", range: { start, end }
-//   });
-
-// } else {
-//   stream = ytdl(videoURL, {
-//     quality: "highestaudio",
-//     filter: "audioonly"
-//   });
-// }
-
-//   const audio = ytdl(videoURL, { quality: 'highestaudio' })
-//     .on('progress', (_, downloaded, total) => {
-//       console.log(downloaded, total);
-//       tracker.audio = { downloaded, total };
-//     });
-//     const video = ytdl(videoURL, { quality: 'highestvideo' })
-//   .on('progress', (_, downloaded, total) => {
-//   tracker.video = { downloaded, total };
-// });
-//   let progressbarHandle = null;
-//   const progressbarInterval = 1000;
-//   const showProgress = () => {
-//     readline.cursorTo(process.stdout, 0);
-//     const toMB = i => (i / 1024 / 1024).toFixed(2);
-
-//     process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
-//     process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
-
-//     process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
-//     process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
-
-//     process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
-//     process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
-
-//     process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
-//     readline.moveCursor(process.stdout, 0, -3);
-//   };
-
-//   // Start the ffmpeg child process
-//   const ffmpegProcess = cp.spawn(ffmpeg, [
-//     // Remove ffmpeg's console spamming
-//     '-loglevel', '8', '-hide_banner',
-//     // Redirect/Enable progress messages
-//     '-progress', 'pipe:3',
-//     // Set inputs
-//     '-i', 'pipe:4',
-//     '-i', 'pipe:5',
-//     // Map audio & video from streams
-//     '-map', '0:a',
-//     '-map', '1:v',
-//     // Keep encoding
-//     '-c:v', 'copy',
-//     // Define output file
-//     'out.mkv',
-//   ], {
-//     windowsHide: true,
-//     stdio: [
-//       /* Standard: stdin, stdout, stderr */
-//       'inherit', 'inherit', 'inherit',
-//       /* Custom: pipe:3, pipe:4, pipe:5 */
-//       'pipe', 'pipe', 'pipe',
-//     ],
-//   });
-//   ffmpegProcess.on('close', () => {
-//     console.log('done');
-//     // Cleanup
-//     process.stdout.write('\n\n\n\n');
-//     clearInterval(progressbarHandle);
-//   });
-
-//   // Link streams
-//   // FFmpeg creates the transformer streams and we just have to insert / read data
-//   ffmpegProcess.stdio[3].on('data', chunk => {
-//     // Start the progress bar
-//     if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
-//     // Parse the param=value list returned by ffmpeg
-//     const lines = chunk.toString().trim().split('\n');
-//     const args = {};
-//     for (const l of lines) {
-//       const [key, value] = l.split('=');
-//       args[key.trim()] = value.trim();
-//     }
-//     tracker.merged = args;
-//   });
-//   audio.pipe(ffmpegProcess.stdio[4]);
-//   video.pipe(ffmpegProcess.stdio[5]);
-
-
-// if (totalSize > chunkSize) {
-//   console.log('--greater than 24mb');
-//   let start = 0;
-//   let end = chunkSize;
-//   let chunkIndex = 1;
-
-//   // Download chunks until the entire video is downloaded
-//   for (let i = 0; i < Math.round(totalSize / chunkSize); i++) {
-//     const chunkname = `chunk_${chunkIndex}.mp3`
-//     transferVideo(chunkname, start, end, true).then(async (d) => {
-//       console.log(d);
-//       // if (d) {
-//       //   console.log(fs.createReadStream(d));
-//       //   const transcription = await openai.audio.transcriptions.create({
-//       //     model: 'whisper-1',
-//       //     file: fs.createReadStream(d),
-//       //   })
-//       //   console.log('---transcription big---', transcription);
-
-//       // }
-
-//     })
-//     start = end + 1;
-//     end = Math.min(start + chunkSize, totalSize);
-//     chunkIndex++;
-
-//   }
-
-
-// } else {
-//   console.log('--less than 23mb');
-
-//   const chunkname = `chunk_1.mp3`
-//   transferVideo(chunkname, 0, 0, false).then(async (d) => {
-//     console.log(d);
-//     // const transcription = await openai.audio.transcriptions.create({
-//     //   model: 'whisper-1',
-//     //   file: fs.createReadStream(chunkname),
-//     // })
-//     // console.log('---transcription small---', transcription);
-
-//   })
-// }
